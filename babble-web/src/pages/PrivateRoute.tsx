dcd2fb87da6dde7fc3dea081b8a5b3cae94f9c92
Yumin22224 <yumin224@snu.ac.kr>
2024-02-18 07:55:26 +0900

    baseUrl

--- babble-web/src/pages/PrivateRoute.tsx
+++ babble-web/src/pages/PrivateRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const token = localStorage.getItem("accessToken");
   if (token) {
-    console.log("accessToken yes");
+    console.log("accessToken yes", token);
   } else {
     console.log("accessToken no");
   }
